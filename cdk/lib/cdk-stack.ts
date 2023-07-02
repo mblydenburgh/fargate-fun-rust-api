b1511c5f51a10e67b40fbeafdbe8f97603a77b2c
Michael Blydenburgh <michael.blydenburgh@libertymutual.com>
2023-07-02 12:01:50 -0400

    import vpc manually and update cluster lookup

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { Cluster, ContainerImage } from 'aws-cdk-lib/aws-ecs';
 import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
@@ -8,7 +9,15 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const cluster = Cluster.fromClusterArn(this, "ClusterRef", "arn:aws:ecs:us-east-1:415023725722:cluster/test_cluster")
+    const vpc = Vpc.fromLookup(this, "VpcReference", {
+      vpcId: "vpc-0ed968b03f671abf6"
+    })
+
+    const cluster = Cluster.fromClusterAttributes(this, "ClusterRef", {
+      clusterName: "test_cluster",
+      clusterArn: "arn:aws:ecs:us-east-1:415023725722:cluster/test_cluster",
+      vpc
+    })
 
     new ApplicationLoadBalancedFargateService(this, "TestFargateService", {
       cluster,
