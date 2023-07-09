00bb0e06a2b3429cfe6c8b3fbfc1b4d5b08b9ac9
Michael Blydenburgh <michael.blydenburgh@libertymutual.com>
2023-07-09 10:38:55 -0400

    imports shared SG, sets task definition port and health check port manually

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { Peer, Port, SecurityGroup, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { SecurityGroup, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { Cluster, ContainerImage } from 'aws-cdk-lib/aws-ecs';
 import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
@@ -15,27 +15,22 @@ export class CdkStack extends cdk.Stack {
 
     const cluster = Cluster.fromClusterAttributes(this, "ClusterRef", {
       clusterName: "fargate-fun-cluster",
-      clusterArn: "arn:aws:ecs:us-east-1:415023725722:cluster/test_cluster",
+      clusterArn: `arn:aws:ecs:us-east-1:${cdk.Aws.ACCOUNT_ID}:cluster/fargate-fun-cluster`,
       vpc
     })
 
-    const securityGroup = new SecurityGroup(this, "ServiceSG", {
-      securityGroupName: "fargate-fun-sg",
-      vpc,
-      allowAllOutbound: true
-    })
-    securityGroup.addEgressRule(Peer.anyIpv4(), Port.tcp(80))
+    const securityGroup = SecurityGroup.fromLookupById(this, "SGRef", "sg-04a436db0880c3f3a")
 
     const service = new ApplicationLoadBalancedFargateService(this, "TestRustFargateService", {
       cluster,
-      loadBalancerName: "fargate-fun-alb",
+      loadBalancerName: "fargate-fun-loadbalancer",
       cpu: 256,
       securityGroups: [securityGroup],
       desiredCount: 1,
       memoryLimitMiB: 512,
       taskImageOptions: {
         image: ContainerImage.fromEcrRepository(
-          Repository.fromRepositoryName(this, "ImageRepository", "image_repository"),
+          Repository.fromRepositoryName(this, "ImageRepository", process.env.REPO_NAME || "bogus"),
           "latest"
         ),
         containerPort: 80
